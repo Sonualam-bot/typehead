3d00ce9825b6bf24f5450dcfd245af04b082fac7
Sonu <sonualam1599@gmail.com>
2023-07-21 15:03:27 +0530

    typehead functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { ListBox } from "./searchBox/ListBox";
+import { Search } from "./searchBox/Search";
+import "./App.css";
 
-function App() {
+const maxItems = 5;
+export default function App() {
+  const transformData = (data) => data.results.slice(0, maxItems);
+  const dataPromise = async (query, signal) =>
+    await fetch(`https://swapi.dev/api/people/?search=${query}`, { signal });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Search
+        id="personName"
+        name="personName"
+        label="Enter Person Name"
+        placeholder="Enter Your Favourite Star war Character"
+        autoComplete={true}
+        maxItems={5}
+        styles={{
+          label: "label",
+          input: "input"
+        }}
+        debounceWait={400}
+        listBox={(items, activeIndex) => (
+          <ListBox items={items} activeIndex={activeIndex} />
+        )}
+        noItemMessage={() => <div> Sorry no person found</div>}
+        errorMessage={() => <div>Something went wrong</div>}
+        transformData={transformData}
+        promise={dataPromise}
+      />
     </div>
   );
 }
-
-export default App;
